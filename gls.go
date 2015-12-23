42a06e07d36059b548b6afe81650b05531a3ea22
Kelsey Hightower <kelsey.hightower@gmail.com>
2015-12-23 06:54:36 -0500

    move Ls function to the gls package

--- gls.go
+++ gls.go
@@ -1,5 +1,10 @@
 package gls
 
+import (
+	"os"
+	"path/filepath"
+)
+
 type Files []File
 
 type File struct {
@@ -7,4 +12,31 @@ type File struct {
 	Size    int64
 	Mode    string
 	ModTime string
+}
+
+type Ls struct{}
+
+func (ls *Ls) Ls(path *string, files *Files) error {
+	root := *path
+	err := filepath.Walk(*path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		file := File{
+			info.Name(),
+			info.Size(),
+			info.Mode().String(),
+			info.ModTime().Format("Jan _2 15:04"),
+		}
+		*files = append(*files, file)
+		if info.IsDir() && path != root {
+			return filepath.SkipDir
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- server/main.go
+++ server/main.go
@@ -4,42 +4,13 @@ import (
 	"log"
 	"net"
 	"net/rpc"
-	"os"
-	"path/filepath"
 
 	"github.com/kelseyhightower/gls"
 )
 
-type Ls struct{}
-
-func (ls *Ls) Ls(path *string, files *gls.Files) error {
-	root := *path
-	err := filepath.Walk(*path, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		file := gls.File{
-			info.Name(),
-			info.Size(),
-			info.Mode().String(),
-			info.ModTime().Format("Jan _2 15:04"),
-		}
-		*files = append(*files, file)
-		if info.IsDir() && path != root {
-			return filepath.SkipDir
-		}
-		return nil
-	})
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
 func main() {
 	log.Println("Starting glsd..")
-	ls := new(Ls)
+	ls := new(gls.Ls)
 	rpc.Register(ls)
 	l, err := net.Listen("tcp", "0.0.0.0:8080")
 	if err != nil {
